f897a417498126dbdea1df6bc3955ee918ddec6a
Vikash <vikashdas770@gmail.com>
2025-01-15 16:25:30 +0530

    cicd run - 13

--- app.py
+++ app.py
@@ -1,12 +1,36 @@
-# app.py
+# # app.py
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return "Hello, World! Welcome to the Flask App deployed on EKS!"
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)
+
+
 from flask import Flask
+from prometheus_client import start_http_server, Counter, Gauge, generate_latest
+from flask import Response
 
 app = Flask(__name__)
 
+# Metrics
+REQUEST_COUNT = Counter('flask_app_requests_total', 'Total number of requests')
+ACTIVE_REQUESTS = Gauge('flask_app_active_requests', 'Number of active requests')
+
 @app.route('/')
 def home():
+    REQUEST_COUNT.inc()  # Increment the request count
     return "Hello, World! Welcome to the Flask App deployed on EKS!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route('/metrics')
+def metrics():
+    return Response(generate_latest(), mimetype="text/plain")
 
+if __name__ == '__main__':
+    # Start Prometheus metrics server
+    start_http_server(5001)  # Default Prometheus metrics exposed on port 5001
+    app.run(host='0.0.0.0', port=5000)
